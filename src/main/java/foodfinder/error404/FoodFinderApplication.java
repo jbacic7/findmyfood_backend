09875816971108e306e69523bb95d4559b918834
jbacic2 <jurica.bacic@t.ht.hr>
2019-09-18 12:48:02 +0200

    Application rename, git ignore.

--- src/main/java/foodfinder/error404/FoodFinderApplication.java
+++ src/main/java/foodfinder/error404/FoodFinderApplication.java
@@ -3,12 +3,13 @@ package foodfinder.error404;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Error404Application {
+public class FoodFinderApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Error404Application.class, args);
+        SpringApplication.run(FoodFinderApplication.class, args);
     }
 
 
