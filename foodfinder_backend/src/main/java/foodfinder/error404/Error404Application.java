069b94b0a61398ebf259b863f908221c0a0609c6
foodfinder7 <55444119+foodfinder7@users.noreply.github.com>
2019-09-18 10:32:31 +0200

    Add files via upload
    Project started

---
+++ foodfinder_backend/src/main/java/foodfinder/error404/Error404Application.java
@@ -0,0 +1,15 @@
+package foodfinder.error404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Error404Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Error404Application.class, args);
+    }
+
+
+}
---
+++ foodfinder_backend/src/test/java/foodfinder/error404/Error404ApplicationTests.java
@@ -0,0 +1,16 @@
+package foodfinder.error404;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Error404ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
