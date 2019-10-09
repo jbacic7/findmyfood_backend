411111b04b613845a2dcdbcc9e7fea087f0e15c2
jbacic7 <jurica.bacic@t.ht.hr>
2019-10-09 09:01:35 +0200

    Added Type class.

---
+++ src/main/java/foodfinder/dto/Type.java
@@ -0,0 +1,33 @@
+package foodfinder.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@NotNull
+@Table(name = "type")
+public class Type {
+
+    @Id
+    @Column(name = "type_Id")
+    private Integer typeId;
+    private String name;
+    @Nullable
+    private String description;
+
+}
