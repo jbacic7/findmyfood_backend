50f99cbfc1313ca4acad196ff19ef857476f4f72
jbacic2 <jurica.bacic@t.ht.hr>
2020-01-31 10:43:30 +0100

    endpoint for average rate of restaurants.

--- src/main/java/foodfinder/repository/GradeRepository.java
+++ src/main/java/foodfinder/repository/GradeRepository.java
@@ -16,5 +16,4 @@ public interface GradeRepository extends JpaRepository<RestaurantGrade, Integer>
     @Transactional
     @Query(value = "SELECT AVG(rg.grade) FROM restaurants_grade rg WHERE rg.id_restaurants = :restaurantId", nativeQuery = true)
     Double countRestaurantGrade(@Param("restaurantId") Integer restaurantId);
-
 }
