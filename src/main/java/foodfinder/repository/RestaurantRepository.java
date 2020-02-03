4a69c17e71859f7d52d036e615448735e21aee53
jbacic2 <jurica.bacic@t.ht.hr>
2020-02-03 14:09:08 +0100

    endpoint for average rate of restaurants.

--- src/main/java/foodfinder/repository/RestaurantRepository.java
+++ src/main/java/foodfinder/repository/RestaurantRepository.java
@@ -17,4 +17,5 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>
     Restaurant findRestaurantByLongitude(Float longitude);
     List<Restaurant> findRestaurantsByTypeIn(List<String> typeList);
 
+
 }
